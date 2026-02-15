84286749e4243bda2b9fbd9b9435cad28932decc
Thibaut RAIMOND <raimond.thibaut@gmail.com>
2026-02-15 18:32:06 +0100

    fix: HashRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
+const basePath = "/portfolio";
+const { pathname, search, hash } = window.location;
+
+if (!hash) {
+	const normalizedPath = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
+	const shouldRedirect =
+		normalizedPath && normalizedPath !== "/" && normalizedPath !== basePath;
+
+	if (shouldRedirect) {
+		const routePath = normalizedPath.startsWith(basePath + "/")
+			? normalizedPath.slice(basePath.length)
+			: normalizedPath;
+		const newPathname = normalizedPath.startsWith(basePath) ? basePath + "/" : "/";
+		window.history.replaceState(null, "", `${newPathname}#${routePath}${search}`);
+	}
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter basename="/portfolio">
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</React.StrictMode>
 );
